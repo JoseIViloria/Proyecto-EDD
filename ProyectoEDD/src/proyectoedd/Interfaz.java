41138ad68e918b206eefcf84a59e8e0645884fb4
LuisEVW <luisviloria@correo.unimet.edu.ve>
2025-10-24 18:43:58 -0400

    Añadida función para abrir y leer un archivo

--- ProyectoEDD/src/proyectoedd/Interfaz.java
+++ ProyectoEDD/src/proyectoedd/Interfaz.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package proyectoedd;
-
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
 /**
  *
  * @author Gemelos
@@ -49,7 +52,7 @@ public class Interfaz extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
@@ -69,7 +72,26 @@ public class Interfaz extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> new Interfaz().setVisible(true));
     }
-
+        public String abrirarchivo(){
+            String aux = "";
+            String texto="";
+            try{
+                JFileChooser elegir_archivo = new JFileChooser();
+                elegir_archivo.showOpenDialog(this);
+                File abrir = elegir_archivo.getSelectedFile();
+                if (abrir!=null){
+                    FileReader archivo=new FileReader(abrir);
+                    BufferedReader leer=new BufferedReader(archivo);
+                    while((aux=leer.readLine())!=null){
+                        texto+= aux + "\n";
+                        }
+                    leer.close();
+            }
+            }catch(Exception a){
+                return "Ocurrió un error al intentar leer el archivo";
+            }
+            return texto;
+        }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }

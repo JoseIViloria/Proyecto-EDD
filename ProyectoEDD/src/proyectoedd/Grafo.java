d2cc3dd55aaa318a541a8db632cce0dea7170b7e
José Viloria <joseviloria@correo.unimet.edu.ve>
2025-10-23 22:53:52 -0400

    Grafo - constructor y métodos iniciales

--- ProyectoEDD/src/proyectoedd/Grafo.java
+++ ProyectoEDD/src/proyectoedd/Grafo.java
@@ -9,5 +9,43 @@ package proyectoedd;
  * @author Gemelos
  */
 public class Grafo {
+    private int maxNodos;
+    private int vertices;
+    private Lista ady[];
     
+    public Grafo(int n){
+        this.maxNodos = n;
+        this.vertices = 0;
+        ady = new Lista[n];
+    }
+
+    /**
+     * @return the maxNodos
+     */
+    public int getMaxNodos() {
+        return maxNodos;
+    }
+
+    public void setMaxNodos(int maxNodos) {
+        this.maxNodos = maxNodos;
+    }
+
+
+    public int getVertices() {
+        return vertices;
+    }
+
+
+    public void setVertices(int vertices) {
+        this.vertices = vertices;
+    }
+    
+    public Lista getLista(int x){
+        if (x>this.getMaxNodos()){
+            return null;
+        }
+        else{
+            return ady[x];
+        }
+    }
 }

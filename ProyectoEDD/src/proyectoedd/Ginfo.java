c51773076ad9b93c0642b2dadd0c7c2f39948bfe
José Viloria <joseviloria@correo.unimet.edu.ve>
2025-11-01 01:03:44 -0400

    Javadoc de la clase Ginfo

--- ProyectoEDD/src/proyectoedd/Ginfo.java
+++ ProyectoEDD/src/proyectoedd/Ginfo.java
@@ -5,13 +5,18 @@
 package proyectoedd;
 
 /**
- *
- * @author Gemelos
+ * Clase que permite crear un contenedor para los datos extraídos del archivo de texto.
+ * Contiene un array para almacenar los usuarios y otro (una matriz) para las relaciones. 
+ * @see Interfaz
+ * @author José Viloria
  */
 public class Ginfo {
     public String[] usuarios;
     public String[][] relaciones;
 
+    /**
+     * Constructor de la clase Ginfo.
+     */
     public Ginfo() {
         this.usuarios = usuarios;
         this.relaciones = relaciones;
--- ProyectoEDD/src/proyectoedd/ProyectoEDD.java
+++ ProyectoEDD/src/proyectoedd/ProyectoEDD.java
@@ -6,7 +6,7 @@ package proyectoedd;
 
 /**
  *
- * @author Gemelos
+ * @author 
  */
 public class ProyectoEDD {
 
